22533e291465a4d1886946542fb9bde9452ae4d5
tilak-atpl <tilak.ram@autopeepal.com>
2024-08-06 11:48:42 +0530

    this is imporant to have vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  server: { //this is imporant to have, To tell ec2 instance that default port 3000 always
     port: 3000,
     host: '0.0.0.0',
   },
