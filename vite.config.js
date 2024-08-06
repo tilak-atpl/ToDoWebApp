98b0b1a4816e2aa015c69cc3c7cffe79826c7bcb
tilak-atpl <tilak.ram@autopeepal.com>
2024-08-06 11:41:36 +0530

    just to check wether previouse version code will work or not, back to vite config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   port: 3000,
-  //   host: '0.0.0.0',
-  // },
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
 })
+  
